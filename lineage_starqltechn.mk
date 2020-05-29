a67724baf72764280781c62b2131f91f5f8b3f30
klabit87 <keith.labit@gmail.com>
2020-05-29 15:23:04 -0500

    Import fp patches from star2qltechn

--- lineage_starqltechn.mk
+++ lineage_starqltechn.mk
@@ -6,13 +6,6 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_NAME := lineage_starqltechn
 BOARD_VENDOR := samsung
 
-# Build fingerprint
-ifneq ($(SIGN_BUILD),true)
-PRODUCT_BUILD_PROP_OVERRIDES += \
-	BUILD_FINGERPRINT=samsung/starqltezh/starqltechn:10/QP1A.190711.020/G9600ZHU6DTA7:user/release-keys \
-	PRIVATE_BUILD_DESC="starqltezh-user 10 QP1A.190711.020 G9600ZHU6DTA7 release-keys"
-endif
-
 # OTA
 PRODUCT_PROPERTY_OVERRIDES += \
 	lineage.updater.uri=https://raw.githubusercontent.com/klabit87/OTA/master/starqltechn.json
